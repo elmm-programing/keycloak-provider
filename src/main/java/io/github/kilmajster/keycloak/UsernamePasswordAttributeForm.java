e9255c61134bb0d296cfb9d95ad8280a0cfbd245
ELMM <elmm.programing@gmail.com>
2024-07-28 18:42:04 -0400

    Last functional

--- src/main/java/io/github/kilmajster/keycloak/UsernamePasswordAttributeForm.java
+++ src/main/java/io/github/kilmajster/keycloak/UsernamePasswordAttributeForm.java
@@ -2,24 +2,22 @@ package io.github.kilmajster.keycloak;
 
 import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
+
+import static io.github.kilmajster.keycloak.UsernamePasswordAttributeFormConfiguration.USER_ATTRIBUTE;
+import static io.github.kilmajster.keycloak.UsernamePasswordAttributeFormConfiguration.configPropertyOf;
+
 import org.keycloak.authentication.AuthenticationFlowContext;
-import org.keycloak.authentication.AuthenticationFlowError;
+
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.authentication.authenticators.browser.UsernamePasswordForm;
-import org.keycloak.events.Details;
-import org.keycloak.events.Errors;
-import org.keycloak.forms.login.LoginFormsProvider;
+
 import org.keycloak.models.UserModel;
-import org.keycloak.models.UserProvider;
-import org.keycloak.models.utils.FormMessage;
+
 import org.keycloak.services.ServicesLogger;
 
-import static io.github.kilmajster.keycloak.UsernamePasswordAttributeFormConfiguration.*;
-import static org.keycloak.services.validation.Validation.FIELD_PASSWORD;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
+
+
 
 public class UsernamePasswordAttributeForm extends UsernamePasswordForm implements Authenticator {
 
@@ -27,16 +25,12 @@ public class UsernamePasswordAttributeForm extends UsernamePasswordForm implemen
 
     @Override
     protected Response challenge(AuthenticationFlowContext context, String error, String field) {
-        setUserAttributeFormLabel(context);
-        setUserAttributeFormErrorMessage(context);
 
         return super.challenge(context, null, null);
     }
 
     @Override
     protected Response challenge(AuthenticationFlowContext context, MultivaluedMap<String, String> formData) {
-        setUserAttributeFormLabel(context);
-
         return super.challenge(context, formData);
     }
 
@@ -45,7 +39,8 @@ public class UsernamePasswordAttributeForm extends UsernamePasswordForm implemen
     log.info("Form Data");
     log.info(formData.toString());
     String username =context.getHttpRequest().getDecodedFormParameters().getFirst("username");
-    UserModel user = context.getSession().users().searchForUserByUserAttributeStream(context.getRealm(), "cedula",username ).findFirst().orElse(null);
+    String userAttributeName = configPropertyOf(context, USER_ATTRIBUTE);
+    UserModel user = context.getSession().users().searchForUserByUserAttributeStream(context.getRealm(), userAttributeName,username ).findFirst().orElse(null);
     if (user != null) {
         log.info("user found by cedula:");
         log.info(user.getAttributes().toString());
@@ -53,94 +48,5 @@ public class UsernamePasswordAttributeForm extends UsernamePasswordForm implemen
         formData.add("username", user.getUsername());
     }
     return super.validateForm(context, formData);
-    }
-
-    private void setUserAttributeFormLabel(AuthenticationFlowContext context) {
-        String userAttributeLabel = configPropertyOf(context, USER_ATTRIBUTE_LABEL);
-
-        // label
-        if (userAttributeLabel != null) {
-            context.form().setAttribute(USER_ATTRIBUTE_LABEL, userAttributeLabel);
-        } else {
-            String userAttributeName = configPropertyOf(context, USER_ATTRIBUTE);
-            if (userAttributeName != null && !userAttributeName.isEmpty()) {
-                context.form().setAttribute(
-                        USER_ATTRIBUTE_LABEL,
-                        isGenerateLabelEnabled(context)
-                                ? generateLabel(userAttributeName, true)
-                                : userAttributeName
-                );
-            } else {
-                log.warn("Configuration of keycloak-user-attribute-authenticator is incomplete! " +
-                        "At least user_attribute property needs to be set!");
-            }
-        }
-    }
-
-    private void setUserAttributeFormErrorMessage(AuthenticationFlowContext context) {
-        String userAttributeName = configPropertyOf(context, USER_ATTRIBUTE);
-        String userAttributeErrorMessage = configPropertyOf(context, USER_ATTRIBUTE_ERROR_MESSAGE);
-
-        if (userAttributeErrorMessage != null) {
-            context.form().addError(
-                    new FormMessage(FIELD_PASSWORD, userAttributeErrorMessage, userAttributeName)
-            );
-        } else {
-            if (userAttributeName != null && !userAttributeName.isEmpty()) {
-                context.form().addError(
-                        new FormMessage(
-                                FIELD_PASSWORD,
-                                "invalidUsernamePasswordOrAttributeMessage",
-                                isGenerateLabelEnabled(context)
-                                        ? generateLabel(userAttributeName, false)
-                                        : userAttributeName
-                        )
-                );
-            } else {
-                log.warn("Configuration of keycloak-user-attribute-authenticator is incomplete! " +
-                        "At least user_attribute property needs to be set!");
-            }
-        }
-    }
-
-    private boolean isUserAttributeValid(AuthenticationFlowContext context, String providedAttribute) {
-        String attributeName = context.getAuthenticatorConfig().getConfig().get(USER_ATTRIBUTE);
-        UserModel user = context.getUser();
-        boolean attributeValid = user != null
-                && user.getAttributeStream(attributeName).anyMatch(attr -> attr.equals(providedAttribute));
-
-        return attributeValid || badAttributeHandler(context, user);
-    }
-
-    private boolean badAttributeHandler(AuthenticationFlowContext context, UserModel user) {
-        context.getEvent().user(user);
-        context.getEvent().error(Errors.INVALID_USER_CREDENTIALS);
-        context.getEvent().detail(Details.REASON, "Invalid user attribute was provided");
-
-        if (isUserAlreadySetBeforeUsernamePasswordAuth(context)) {
-            LoginFormsProvider form = context.form();
-            form.setAttribute(LoginFormsProvider.USERNAME_HIDDEN, true);
-            form.setAttribute(LoginFormsProvider.REGISTRATION_DISABLED, true);
-        }
-
-        Response challengeResponse = challenge(context, getDefaultChallengeMessage(context), FIELD_PASSWORD);
-        context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, challengeResponse);
-        context.clearUser();
-
-        return false;
-    }
-
-    private String generateLabel(final String attributeName, boolean capitalize) {
-        final String lowercaseWithSpaces = attributeName
-                .toLowerCase()
-                .replace(".", " ")
-                .replace("_", " ")
-                .replace("-", " ");
-
-        return capitalize ? capitalize(lowercaseWithSpaces) : lowercaseWithSpaces;
-    }
-
-    private String capitalize(final String toCapitalize) {
-        return toCapitalize.substring(0, 1).toUpperCase() + toCapitalize.substring(1).toLowerCase();
     }
 }
